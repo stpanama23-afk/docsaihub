45501a9b9fe4536ad7b032eb00e6ebb3a07e6820
Damir <stpanama23@gmail.com>
2026-06-18 01:06:43 +0300

    Deploy website - based on 24a8ac2afd0a357b565672d8a64a44e76cd0ac41

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("C:\\Users\\Da_mrrr\\aihub\\docsaihub\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\Da_mrrr\\aihub\\docsaihub\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Da_mrrr\\aihub\\docsaihub\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Da_mrrr\\aihub\\docsaihub\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Da_mrrr\\aihub\\docsaihub\\src\\css\\custom.css"),
-];
