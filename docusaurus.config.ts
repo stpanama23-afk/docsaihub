93f3f3ca84f9d9716d9ac80ccc72ba01e8123aee
Damir <stpanama23@gmail.com>
2026-06-22 19:37:58 +0300

    настройки сайта

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DocsAI Hub',
+  tagline: 'Документация ИИ Хаб',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
